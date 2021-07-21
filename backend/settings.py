7c19eec974ca08f058edf972938001a73adf19da
skjoon1804 <skjoon1804@naver.com>
2021-07-21 01:37:35 -0700

    Update path

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/urls.py
+++ backend/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from api.views import yelp, yelpdeep
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('yelp/', yelp, name="yelp"),
-    path('yelpdeep/', yelpdeep, name="yelpdeep")
+    path('yelpdeep/', yelpdeep, name="yelpdeep"),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
